0def897d50c10db7b3f92fa5a08cef63c218eae7
ioannisdaratzikis <ioannisdaratzikis@gmail.com>
2017-05-05 08:43:51 +0300

    Initial commit

---
+++ app/src/main/java/com/tappgames/sunshine/Controller/SunshineController.java
@@ -0,0 +1,62 @@
+package com.tappgames.sunshine.Controller;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.tappgames.sunshine.Model.Forecast;
+import com.tappgames.sunshine.Rest.Pojos.GetForecastResponse;
+import com.tappgames.sunshine.Rest.RestCallback;
+import com.tappgames.sunshine.Rest.RestClient;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+
+public class SunshineController extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public SunshineController(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+    public void fetchForecast(String lat, String lon, String days, String id, final ForecastComplection complection){
+
+        Call<GetForecastResponse> call = RestClient.call().getForecast("35", "139","10","f978537a55e9535979fc541fefcce46d");
+        call.enqueue(new RestCallback<GetForecastResponse>() {
+            @Override
+            public void handleExeption(Throwable exception) {
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void handleSuccess(GetForecastResponse response) {
+                ArrayList<Forecast> forecasts = response.getList();
+                complection.onResponse(forecasts);
+            }
+
+            @Override
+            public void handleFailure(int statusCode, String statusMessage) {
+                Log.d("ERROR", "failure");
+            }
+        });
+
+    }
+
+    public interface ForecastComplection {
+        void onResponse(ArrayList<Forecast> complection);
+    }
+
+}
